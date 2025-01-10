26609520ca838bff4a01367b4dd02f79bf06d152
maan909 <110489675+maan909@users.noreply.github.com>
2025-01-11 01:26:34 +0530

    second commit it is just for testing

---
+++ Level_3/rotated_array.py
@@ -0,0 +1,9 @@
+def rotated_slicing(k: int, num: list):
+ return 0
+
+my_list = [1, 2, 3, 4, 5,6,7]
+reversed_list = my_list[:4:]
+reversed_list_2 = my_list[4:7:]
+print(reversed_list)
+print(reversed_list_2)
+print(reversed_list_2 + reversed_list)          
